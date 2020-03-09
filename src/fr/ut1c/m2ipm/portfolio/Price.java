a17b2b32349a34ef8f64dd8de553d60a9ec715bc
21701558 <21701558@a17742.ad.ut-capitole.fr>
2020-03-09 10:24:56 +0100

    Update javadoc

--- src/fr/ut1c/m2ipm/portfolio/Price.java
+++ src/fr/ut1c/m2ipm/portfolio/Price.java
@@ -1,21 +1,39 @@
 package fr.ut1c.m2ipm.portfolio;
-
+/**
+ * A class price that indicate a daily price.
+ * @author 21701558
+ */
 public class Price {
+/**
+ * Attribute day which is a Day object.
+ */
+    private final Day day;
+/**
+ * Attribute value which is a float.
+ */
+    private final float value;
 
-    private Day day;
-
-    private float value;
-
-    public float getValue() {
+    /**
+     * Return the object value.
+     * @return Return the object value
+     */
+    public final float getValue() {
         return value;
     }
-
-    public Day getDay() {
+    /**
+     * Return the object day.
+     * @return this day object
+     */
+    public final Day getDay() {
         return day;
     }
-
-    public Price(Day day, float value) {
-        this.day = day;
-        this.value = value;
+    /**
+     * Build a price with two parameters.
+     * @param dayP the day of the price
+     * @param valueP the value of the price
+     */
+    public Price(final Day dayP, final float valueP) {
+        this.day = dayP;
+        this.value = valueP;
     }
 }
