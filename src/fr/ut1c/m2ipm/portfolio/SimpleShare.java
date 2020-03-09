757f475acddbc0963e171523cd4475c9b755e2eb
21709520 <21709520@a17738.ad.ut-capitole.fr>
2020-03-09 10:36:55 +0100

    A Part of SimpleShare commit

--- src/fr/ut1c/m2ipm/portfolio/SimpleShare.java
+++ src/fr/ut1c/m2ipm/portfolio/SimpleShare.java
@@ -2,22 +2,40 @@ package fr.ut1c.m2ipm.portfolio;
 
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * Class of SimpleShare which is the heres of Share.
+ * @author 21709520
+ */
 public class SimpleShare extends Share {
 
+/**
+ * HashMap of price of simple Share : Day is key and Price is value.
+ */
     private Map<Day, Price> mapPrices;
 
-    public SimpleShare(String name) {
+/**
+ * Constractor of Simple Share.
+ * @param name parameter which is the identifiant of Simple Share.
+ */
+    public SimpleShare(final String name) {
         super(name);
         this.mapPrices = new HashMap();
     }
-
+/**
+ * 
+ * @param day
+ * @param value 
+ */
     public void setPrice(Day day, float value) {
         if (this.mapPrices.containsKey(day) == false) {
             this.mapPrices.put(day, new Price(day, value));
         }
     }
-
+/**
+ * 
+ * @param day
+ * @return 
+ */
     @Override
     public float getPrice(Day day) {
         if (this.mapPrices.containsKey(day) == true) {
