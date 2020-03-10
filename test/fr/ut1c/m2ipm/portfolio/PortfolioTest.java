7450f06450495b7a44479cc9ba9b5a59f6ec8a1f
navarre <navarre@A16807.ad.ut-capitole.fr>
2020-03-10 11:56:42 +0100

    Configure Branch name for sonar

--- test/fr/ut1c/m2ipm/portfolio/PortfolioTest.java
+++ test/fr/ut1c/m2ipm/portfolio/PortfolioTest.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.ut1c.m2ipm.portfolio;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- *
- * @author David
- */
 public class PortfolioTest {
 
     public PortfolioTest() {
